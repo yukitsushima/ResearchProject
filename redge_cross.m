1611eb2e1b5f2258320033efcb677c936e82533c
Yuki Tsushima <43738259+yukitsushima@users.noreply.github.com>
2018-11-13 09:33:37 +0900

    まともに動くレベルに修正

--- redge_cross.m
+++ redge_cross.m
@@ -1,33 +1,33 @@
 function SE = redge_cross(X, y, alpha)
-%REDGE_CROSS リッジ回帰の交差検証を行う
-%   詳細説明をここに記述
+%REDGE_CROSS 繝ｪ繝�繧ｸ蝗槫ｸｰ縺ｮ莠､蟾ｮ讀懆ｨｼ繧定｡後≧
+%   隧ｳ邏ｰ隱ｬ譏弱ｒ縺薙％縺ｫ險倩ｿｰ
 [c1, ~] = size(X); % N * K
 [c2, ~] = size(y); % N * 1
 assert(c1 == c2, 'input dimension might be different');
 devide = 4;
-X1;X2;X3;X4;
-y1;y2;y3;y4;
+X1=zeros(1);X2=zeros(1);X3=zeros(1);X4=zeros(1);
+y1=zeros(1);y2=zeros(1);y3=zeros(1);y4=zeros(1);
 X1 = X(1,:);
 y1 = y(1,:);
-for i = 2 : (c1/devide)
+for i = 2 : round((c1/devide))
     X1 = [X1; X(i,:)];
     y1 = [y1; y(i,:)];
 end
-X2 = X((c1/devide)+1,:);
-y2 = y((c1/devide)+1,:);
-for i = (c1/devide)+2 : (c1*2/devide)
+X2 = X(round(c1/devide)+1,:);
+y2 = y(round(c1/devide)+1,:);
+for i = round(c1/devide)+2 : round(c1*2/devide)
     X2 = [X2; X(i,:)];
     y2 = [y2; y(i,:)];
 end
-X3 = X((c1*2/devide)+1,:);
-y3 = y((c1*2/devide)+1,:);
-for i = (c1*2/devide)+2 : (c1*3/devide)
+X3 = X(round(c1*2/devide)+1,:);
+y3 = y(round(c1*2/devide)+1,:);
+for i = round(c1*2/devide)+2 : round(c1*3/devide)
     X3 = [X3; X(i,:)];
     y3 = [y3; y(i,:)];
 end
-X4 = X((c1*3/devide)+1,:);
-y4 = y((c1*3/devide)+1,:);
-for i = (c1*3/devide)+2 : c1
+X4 = X(round(c1*3/devide)+1,:);
+y4 = y(round(c1*3/devide)+1,:);
+for i = round(c1*3/devide)+2 : c1
     X4 = [X4; X(i,:)];
     y4 = [y4; y(i,:)];
 end
@@ -40,10 +40,9 @@ t124 = calc_regression_coefficient(X124,y124,alpha);
 t134 = calc_regression_coefficient(X134,y134,alpha);
 t234 = calc_regression_coefficient(X234,y234,alpha);
 error = 0;
-error = error + y4 - X4 * t123;
-error = error + y3 - X3 * t124;
-error = error + y2 - X2 * t134;
-error = error + y1 - X1 * t234;
+error = error + sum(y4 - X4 * t123);
+error = error + sum(y3 - X3 * t124);
+error = error + sum(y2 - X2 * t134);
+error = error + sum(y1 - X1 * t234);
 SE = error / 4;
 end
-
